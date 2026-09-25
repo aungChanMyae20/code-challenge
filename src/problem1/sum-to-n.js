1ab45ddf342d1e64d5e1876b6518226e548d359d
aungChanMyae20 <aungchanmyae.dev@gmail.com>
2026-09-25 09:09:09 +0630

    [correct] - function names

--- src/problem1/sum-to-n.js
+++ src/problem1/sum-to-n.js
@@ -1,4 +1,4 @@
-function sumIteration(n) {
+function sum_to_n_a(n) {
     let total = 0;
     for (let i = 1; i <= n; i++) {
         total += i;
@@ -6,19 +6,19 @@ function sumIteration(n) {
     return total;
 }
 
-function sumRecursive(n) {
+function sum_to_n_b(n) {
     if (n <= 0) return 0;
     return n + sumRecursive(n - 1);
 }
 
-function sumFormula(n) {
+function sum_to_n_c(n) {
     return (n * (n + 1)) / 2;
 }
 
 [5, 6, 11, 15, 21, 28, 33, 39, 47, 100].map(n => {
     console.log('n: ', n);
-    console.log('Iteration Result: ', sumIteration(n));
-    console.log('Recursive Result: ', sumRecursive(n));
-    console.log('Formula Result: ', sumFormula(n));
+    console.log('Iteration Result: ', sum_to_n_a(n));
+    console.log('Recursive Result: ', sum_to_n_b(n));
+    console.log('Formula Result: ', sum_to_n_c(n));
     console.log('---------------------');
 });
